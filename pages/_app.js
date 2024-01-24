58c3cc6b5e6873045d12815ac02c6b09c14ed41b
amichaeel <mail@anthonymham.com>
2024-01-24 01:46:36 -0500

    vercel analytics support

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,19 @@
 import "./globals.css"
 import Navbar from "@/components/Navbar"
 import Footer from "@/components/Footer"
+import { Analytics } from "@vercel/analytics/react"
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <div className="flex flex-col h-screen justify-between">
-      <div>
-        <Navbar />
-        <Component {...pageProps} />
+    <>
+      <div className="flex flex-col h-screen justify-between">
+        <div>
+          <Navbar />
+          <Component {...pageProps} />
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+      <Analytics />
+    </>
   )
 }
