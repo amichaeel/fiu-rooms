fa1e661fc1f862d1e3cb11475b16bf9c9aff3977
amichaeel <mail@anthonymham.com>
2024-02-16 17:42:19 -0500

    add: favicon and title

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,16 @@
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import Head from "next/head";
 import { Analytics } from "@vercel/analytics/react";
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <div className="flex min-h-screen flex-col justify-between">
+        <Head>
+          <title>FIU Rooms</title>
+        </Head>
         <div>
           <Navbar />
           <Component {...pageProps} />
