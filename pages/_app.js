1f80d3e8f21728ca1df2e0c33c0aabcbacb9b2fe
amichaeel <mail@anthonymham.com>
2024-02-16 17:48:22 -0500

    add: favicon support for ios and android

--- pages/_app.js
+++ pages/_app.js
@@ -10,6 +10,24 @@ export default function MyApp({ Component, pageProps }) {
       <div className="flex min-h-screen flex-col justify-between">
         <Head>
           <title>FIU Rooms</title>
+          <link rel="shortcut icon" href="/images/favicon.ico" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/images/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/images/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/images/favicon-16x16.png"
+          />
         </Head>
         <div>
           <Navbar />
