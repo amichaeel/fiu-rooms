0b67c1da741c2eb52b3642d329d4cc69c5cea5d5
amichaeel <mail@anthonymham.com>
2024-12-21 15:32:57 -0500

    commit

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="footer footer-center mt-auto bg-primary p-4 text-base-content">
-      <aside className="text-xs">
+      <aside className="text-xs ">
         <p>FIU Rooms is not affiliated with Florida International University</p>
       </aside>
     </footer>
